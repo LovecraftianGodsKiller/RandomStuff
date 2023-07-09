61b9f32829231ee3c5cbfbc0e45f70fbb44c8bb0
LovecraftianGodsKiller <82244192+LovecraftianGodsKiller@users.noreply.github.com>
2023-07-09 07:30:53 -0700

    Update .p10k.zsh

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -189,7 +189,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=67
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
+  # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
